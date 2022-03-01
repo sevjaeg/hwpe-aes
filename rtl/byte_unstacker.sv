e447202b0dfaf1e44802a9b2866d36ea9a3b5dd6
sevjaeg <sev.jaeger@gmail.com>
2022-03-01 21:49:58 +0100

    unstacker latency reduced

--- rtl/byte_unstacker.sv
+++ rtl/byte_unstacker.sv
@@ -13,73 +13,64 @@ module byte_unstacker
   output logic [31:0]                           word_o
 );
 
-logic unsigned [1:0] byte_cnt_r;
+logic unsigned [2:0] cnt_r;
+logic unsigned [2:0] next_cnt;
 logic signed [127:0] stack_r;
-logic signed [31:0] buffer_r;
-logic busy_r;
-logic buffer_free_r;
+logic filled_r;
 
-assign ready_o = (~busy_r) & buffer_free_r;
+assign ready_o = (~filled_r);
 
 always_ff @(posedge clk_i or negedge rst_ni)
 begin : ff
     if(~rst_ni) begin
         stack_r <= '0;
-        byte_cnt_r  <= '0;
-        busy_r <= '0;
-        buffer_r <= '0;
-        valid_o <= 0;
-        buffer_free_r <= 1;
+        cnt_r  <= '0;
+        filled_r <= '0;
     end
     else if (clr_i) begin
         stack_r  <= '0;
-        byte_cnt_r  <= '0;
-        busy_r <= '0;
-        buffer_r <= '0;
-        valid_o <= 0;
-        buffer_free_r <= 1;
+        cnt_r  <= '0;
+        filled_r <= '0;
     end
     else if (enable_i) begin
-        valid_o <= 0;
-        word_o <= 0;
-        if (valid_i & ~busy_r & buffer_free_r) begin
+        if (valid_i & ~filled_r) begin
             stack_r <= word_i;
-            busy_r <= 1;
-            buffer_r <= '0;
-        end else if (busy_r & buffer_free_r) begin
-            case(byte_cnt_r)
-                0: begin
-                    buffer_r <= stack_r[127:96]; 
-                    busy_r <= 1;
-                    byte_cnt_r <= 1; 
-                    buffer_free_r <= 0;          
-                end
-                1: begin
-                    buffer_r <= stack_r[95:64];  
-                    busy_r <= 1;
-                    byte_cnt_r <= 2;
-                    buffer_free_r <= 0;
-                end
-                2: begin
-                    buffer_r <= stack_r[63:32]; 
-                    busy_r <= 1;
-                    byte_cnt_r <= 3;
-                    buffer_free_r <= 0;
-                end
-                3: begin
-                    buffer_r <= stack_r[31:0];
-                    busy_r <= 0;
-                    byte_cnt_r <= 0;
-                    buffer_free_r <= 0;
-                end
-            endcase
+            filled_r <= 1;
         end
-        else if (~buffer_free_r & ready_i) begin
-            word_o <= buffer_r;
-            valid_o <= 1;
-            buffer_free_r <= 1;
+
+        if (next_cnt == 4) begin
+            filled_r <= 0;
+            cnt_r <= 0;
+        end else begin
+            cnt_r <= next_cnt;
         end
     end
 end
+
+always_comb
+begin
+    if (filled_r & ready_i) begin
+        case(cnt_r)
+            0: begin
+                word_o <= stack_r[127:96];
+            end
+            1: begin
+                word_o <= stack_r[95:64];
+            end
+            2: begin
+                word_o <= stack_r[63:32];
+            end
+            3: begin
+                word_o <= stack_r[31:0];
+            end
+        endcase
+        next_cnt <= cnt_r + 1;
+    end else begin
+        next_cnt <= cnt_r;
+        word_o <= '0;
+    end
+end
+
+assign valid_o = filled_r & ready_i;
 
 endmodule // byte_unstacker
