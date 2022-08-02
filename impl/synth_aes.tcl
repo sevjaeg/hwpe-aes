2575fb53840ecb1b0d63aebdbc9332edee4b22fd
Severin Lukas Jaeger <sjaeger@eda03.ict.tuwien.ac.at>
2022-08-02 19:36:16 +0200

    genus scripts added

---
+++ impl/synth_aes.tcl
@@ -0,0 +1,210 @@
+#### Template Script for RTL->Gate-Level Flow (generated from GENUS 19.14-s108_1) 
+
+puts "Hostname : [info hostname]"
+
+##############################################################################
+## Preset global variables and attributes
+##############################################################################
+
+set DESIGN aes_cipher_top
+set GEN_EFF medium
+set MAP_OPT_EFF high
+set DATE [clock format [clock seconds] -format "%b%d-%T"] 
+set _OUTPUTS_PATH out
+set _REPORTS_PATH reports
+set _LOG_PATH logs
+##set ET_WORKDIR <ET work directory>
+set_attribute init_lib_search_path {. /kits/tsmc/65nm/GP_stclib/10-track/tcbn65gplushpbwp-set/tcbn65gplushpbwp_140a_FE/TSMCHOME/digital/Front_End/timing_power_noise/NLDM/tcbn65gplushpbwp_140a/} / 
+set_attribute script_search_path {. } /
+set_attribute init_hdl_search_path {. /home/sjaeger/pulpissimo/ips/hwpe-mac-engine/rtl} /
+##Uncomment and specify machine names to enable super-threading.
+##set_attribute super_thread_servers {<machine names>} /
+##For design size of 1.5M - 5M gates, use 8 to 16 CPUs. For designs > 5M gates, use 16 to 32 CPUs
+##set_attribute max_cpus_per_server 8 /
+
+##Default undriven/unconnected setting is 'none'.  
+##set_attribute hdl_unconnected_input_port_value 0 | 1 | x | none /
+##set_attribute hdl_undriven_output_port_value   0 | 1 | x | none /
+##set_attribute hdl_undriven_signal_value        0 | 1 | x | none /
+
+set_attribute hdl_error_on_blackbox 1
+set_attribute hdl_error_on_latch 1
+
+##set_attribute wireload_mode <value> /
+set_attribute information_level 9 /
+
+###############################################################
+## Library setup
+###############################################################
+
+set_attribute library tcbn65gplushpbwpwc.lib
+## PLE
+set_attribute lef_library /kits/tsmc/65nm/GP_stclib/10-track/tcbn65gplushpbwp-set/tcbn65gplushpbwp_140a_FE/TSMCHOME/digital/Back_End/lef/tcbn65gplushpbwp_140a/lef/tcbn65gplushpbwp_7lmT1.lef /
+
+## Provide either cap_table_file or the qrc_tech_file
+set_attribute cap_table_file /kits/tsmc/65nm/GP_stclib/10-track/tcbn65gplushpbwp-set/tcbn65gplushpbwp_140a_FE/TSMCHOME/digital/Back_End/lef/tcbn65gplushpbwp_140a/techfiles/captable/cln65g+_1p07m+alrdl_rcworst_top1.captable /
+#set_attribute qrc_tech_file <file> /
+##generates <signal>_reg[<bit_width>] format
+#set_attribute hdl_array_naming_style %s\[%d\] /  
+##
+
+# set_attribute lp_insert_clock_gating true /
+
+####################################################################
+## Load Design
+####################################################################
+
+read_hdl aes_sbox.v
+read_hdl aes_rcon.v
+read_hdl aes_key_expand_128.v
+read_hdl aes_cipher_top.v
+
+elaborate $DESIGN
+puts "Runtime & Memory after 'read_hdl'"
+time_info Elaboration
+
+check_design -unresolved
+check_design -all
+
+####################################################################
+## Constraints Setup
+####################################################################
+
+read_sdc aes_cipher_top.sdc
+puts "The number of exceptions is [llength [find /designs/$DESIGN -exception *]]"
+
+#set_attribute force_wireload <wireload name> "/designs/$DESIGN"
+
+if {![file exists ${_LOG_PATH}]} {
+  file mkdir ${_LOG_PATH}
+  puts "Creating directory ${_LOG_PATH}"
+}
+
+if {![file exists ${_OUTPUTS_PATH}]} {
+  file mkdir ${_OUTPUTS_PATH}
+  puts "Creating directory ${_OUTPUTS_PATH}"
+}
+
+if {![file exists ${_REPORTS_PATH}]} {
+  file mkdir ${_REPORTS_PATH}
+  puts "Creating directory ${_REPORTS_PATH}"
+}
+report timing -lint
+
+
+###################################################################################
+## Define cost groups (clock-clock, clock-output, input-clock, input-output)
+###################################################################################
+
+## Uncomment to remove already existing costgroups before creating new ones.
+## rm [find /designs/* -cost_group *]
+
+if {[llength [all::all_seqs]] > 0} { 
+  define_cost_group -name I2C -design $DESIGN
+  define_cost_group -name C2O -design $DESIGN
+  define_cost_group -name C2C -design $DESIGN
+  path_group -from [all::all_seqs] -to [all::all_seqs] -group C2C -name C2C
+  path_group -from [all::all_seqs] -to [all::all_outs] -group C2O -name C2O
+  path_group -from [all::all_inps]  -to [all::all_seqs] -group I2C -name I2C
+}
+
+define_cost_group -name I2O -design $DESIGN
+path_group -from [all::all_inps]  -to [all::all_outs] -group I2O -name I2O
+foreach cg [find / -cost_group *] {
+  report timing -cost_group [list $cg] >> $_REPORTS_PATH/${DESIGN}_pretim.rpt
+}
+
+#### To turn off sequential merging on the design 
+#### uncomment & use the following attributes.
+##set_attribute optimize_merge_flops false /
+##set_attribute optimize_merge_latches false /
+#### For a particular instance use attribute 'optimize_merge_seqs' to turn off sequential merging.
+
+
+####################################################################################################
+## Synthesizing to generic 
+####################################################################################################
+
+set_attribute syn_generic_effort $GEN_EFF /
+syn_generic
+puts "Runtime & Memory after 'syn_generic'"
+time_info GENERIC
+write_snapshot -outdir $_REPORTS_PATH -tag generic
+report datapath > $_REPORTS_PATH/generic/${DESIGN}_datapath.rpt
+report_summary -outdir $_REPORTS_PATH
+
+
+####################################################################################################
+## Synthesizing to gates
+####################################################################################################
+
+set_attribute syn_map_effort $MAP_OPT_EFF /
+syn_map
+puts "Runtime & Memory after 'syn_map'"
+time_info MAPPED
+write_snapshot -outdir $_REPORTS_PATH -tag map
+report_summary -outdir $_REPORTS_PATH
+report datapath > $_REPORTS_PATH/map/${DESIGN}_datapath.rpt
+
+
+foreach cg [find / -cost_group *] {
+  report timing -cost_group [list $cg] > $_REPORTS_PATH/${DESIGN}_[vbasename $cg]_post_map.rpt
+}
+
+
+# write_do_lec -revised_design fv_map -logfile ${_LOG_PATH}/rtl2intermediate.lec.log > ${_OUTPUTS_PATH}/rtl2intermediate.lec.do
+
+## ungroup -threshold <value>
+
+#######################################################################################################
+## Optimize Netlist
+#######################################################################################################
+
+## Uncomment to remove assigns & insert tiehilo cells during Incremental synthesis
+##set_attribute remove_assigns true /
+##set_remove_assign_options -buffer_or_inverter <libcell> -design <design|subdesign> 
+##set_attribute use_tiehilo_for_const <none|duplicate|unique> /
+set_attribute syn_opt_effort $MAP_OPT_EFF /
+syn_opt
+write_snapshot -outdir $_REPORTS_PATH -tag syn_opt
+report_summary -outdir $_REPORTS_PATH
+
+puts "Runtime & Memory after 'syn_opt'"
+time_info OPT
+
+foreach cg [find / -cost_group *] {
+  report timing -cost_group [list $cg] > $_REPORTS_PATH/${DESIGN}_[vbasename $cg]_post_opt.rpt
+}
+
+
+######################################################################################################
+## write backend file set (verilog, SDC, config, etc.)
+######################################################################################################
+
+report datapath > $_REPORTS_PATH/${DESIGN}_datapath_incr.rpt
+report messages > $_REPORTS_PATH/${DESIGN}_messages.rpt
+write_snapshot -outdir $_REPORTS_PATH -tag final
+report_summary -outdir $_REPORTS_PATH
+write_hdl  > ${_OUTPUTS_PATH}/${DESIGN}_synth.v
+## write_script > ${_OUTPUTS_PATH}/${DESIGN}_m.script
+write_sdc > ${_OUTPUTS_PATH}/${DESIGN}_m.sdc
+
+
+#################################
+### write_do_lec
+#################################
+
+# write_do_lec -golden_design fv_map -revised_design ${_OUTPUTS_PATH}/${DESIGN}_m.v -logfile  ${_LOG_PATH}/intermediate2final.lec.log > ${_OUTPUTS_PATH}/intermediate2final.lec.do
+##Uncomment if the RTL is to be compared with the final netlist..
+##write_do_lec -revised_design ${_OUTPUTS_PATH}/${DESIGN}_m.v -logfile ${_LOG_PATH}/rtl2final.lec.log > ${_OUTPUTS_PATH}/rtl2final.lec.do
+
+puts "Final Runtime & Memory."
+time_info FINAL
+puts "============================"
+puts "Synthesis Finished ........."
+puts "============================"
+
+file copy [get_attribute stdout_log /] ${_LOG_PATH}/.
+
+# gui_show
+# quit
