bf92650cdf37ea22fcc92ed0bda6843da9e5d184
sevjaeg <sev.jaeger@gmail.com>
2022-11-22 11:52:15 +0100

    small clean-up

--- scripts/eda.sh
+++
@@ -1,2 +0,0 @@
-sshpass -f ~/eda03 ssh -X sjaeger@eda03.ict.tuwien.ac.at
-
--- scripts/pulpissimo_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p /home/sjaeger/pulpissimo
-sshfs sjaeger@eda03.ict.tuwien.ac.at:/home/sjaeger/pulpissimo /home/sjaeger/pulpissimo -o cache=no,reconnect
-
-source ~/.bashrc
-
